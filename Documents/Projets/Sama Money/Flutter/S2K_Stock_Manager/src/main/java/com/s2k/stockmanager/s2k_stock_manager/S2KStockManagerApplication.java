81749d1f4ec27ff11e3728ae4a07906ca3553951
Seydou <nanouroukou2075@gmail.com>
2022-10-08 19:40:34 +0000

    ...

---
+++ Documents/Projets/Sama Money/Flutter/S2K_Stock_Manager/src/main/java/com/s2k/stockmanager/s2k_stock_manager/S2KStockManagerApplication.java
@@ -0,0 +1,13 @@
+package com.s2k.stockmanager.s2k_stock_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S2KStockManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S2KStockManagerApplication.class, args);
+    }
+
+}
---
+++ Documents/Projets/Sama Money/Flutter/S2K_Stock_Manager/src/test/java/com/s2k/stockmanager/s2k_stock_manager/S2KStockManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s2k.stockmanager.s2k_stock_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S2KStockManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
